4d9a3ae35b79daf4881a3f8152e201abecde62d1
Bergruebe <68869895+Bergruebe@users.noreply.github.com>
2025-04-02 13:51:14 +0200

    Prepare for publish

--- pretix-custom-head/__init__.py
+++ pretix-custom-head/__init__.py
@@ -3,15 +3,15 @@ from pretix.base.plugins import PluginConfig
 
 
 class PluginApp(PluginConfig):
-    name = "pretix_custom_head_tracking"
+    name = "pretix_custom_head"
     verbose_name = _("Custom Head Injection & Plausible Tracking")
 
     class PretixPluginMeta:
         name = _("Custom Head Injection & Plausible Tracking")
-        author = "Dein Name"
+        author = "Bergruebe"
         description = _("Fügt beliebigen Code in den <head> ein und trackt Bestellungen mit Plausible Analytics.")
         visible = True
         version = "1.0.0"
         compatibility = "pretix>=4.0.0"
 
-default_app_config = "pretix_custom_head_tracking.PluginApp"
+default_app_config = "pretix_custom_head.PluginApp"
